ebd2c2b9862a1a03170593b639a41ff482b817b7
KaoruBB <kaorugmbl1234@gmail.com>
2021-10-12 09:06:25 +0900

    def append_sum_row_label

--- df_preprocess.py
+++ df_preprocess.py
@@ -1,14 +1,10 @@
-class FeatureClass():
+# 基本的な操作
+class df_basic():
     
     def __init__(self):
         pass
 
-    # 2つの変数の和を計算する自作関数
-    def my_sum(x,y):
-        z = x + y
-        return z
-
-    # 2つの変数の差を計算する自作関数
-    def my_dif(x,y):
-        z = x - y
-        return z
+    # 最後の行に，合計行を追加
+    def append_sum_row_label(df, sum_col_name="Total"):
+        df.loc[sum_col_name] = df.sum(numeric_only=True)
+        return df
