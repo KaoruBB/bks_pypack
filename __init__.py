a0b0e31e53b6fc58a95055f6c0106e85bd74a168
KaoruBB <kaorugmbl1234@gmail.com>
2021-10-12 09:47:21 +0900

    wip

--- __init__.py
+++ __init__.py
@@ -3,3 +3,8 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from bks_pypack.df_preprocess import df_basic as dbsc
+from bks_pypack.df_preprocess import preprocess_for_plotly as pfp
