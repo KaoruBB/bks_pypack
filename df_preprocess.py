feb44dea1604224bec7bd87ad3822c5e86ead9bf
KaoruBB <kaorugmbl1234@gmail.com>
2021-10-12 09:09:26 +0900

    def add_color_col

--- df_preprocess.py
+++ df_preprocess.py
@@ -8,3 +8,18 @@ class df_basic():
     def append_sum_row_label(df, sum_col_name="Total"):
         df.loc[sum_col_name] = df.sum(numeric_only=True)
         return df
+
+class preprocess_for_plotly():
+    
+    def __init__(self):
+        pass
+
+    # 真ん中の行，最後の行にcolorというcolを追加
+    def add_color_col(
+        df,
+        middlle_c='rgb(239, 243, 255)',
+        bottom_c='rgb(189, 215, 231)'
+        ):
+        df['color'] = middlle_c
+        df.iloc[-1, -1] = bottom_c
+        return df
